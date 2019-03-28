9640ff7fd7a68ece424a7a4f75bbaa28caa33c98
michelle-ew <medhie@student.unimelb.edu.au>
2019-03-28 18:14:19 +1100

    INITIAL/mish

---
+++ info30005_starwars-master/app.js
@@ -0,0 +1,101 @@
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+var people = [
+    {  "name": "Luke Skywalker",
+        "height": "172",
+        "mass": "77",
+        "hair_color": "blond",
+        "skin_color": "fair",
+        "eye_color": "blue",
+        "birth_year": "19BBY",
+        "gender": "male",
+        "homeworld": "https://swapi.co/api/planets/1/",
+        "films": [
+        "https://swapi.co/api/films/2/",
+        "https://swapi.co/api/films/6/",
+        "https://swapi.co/api/films/3/",
+        "https://swapi.co/api/films/1/",
+        "https://swapi.co/api/films/7/"
+    },
+    {
+        "name": "C-3PO",
+        "height": "167",
+        "mass": "75",
+        "hair_color": "n/a",
+        "skin_color": "gold",
+        "eye_color": "yellow",
+        "birth_year": "112BBY",
+        "gender": "n/a",
+        "homeworld": "https://swapi.co/api/planets/1/",
+        "films": [
+        "https://swapi.co/api/films/2/",
+        "https://swapi.co/api/films/5/",
+        "https://swapi.co/api/films/4/",
+        "https://swapi.co/api/films/6/",
+        "https://swapi.co/api/films/3/",
+        "https://swapi.co/api/films/1/"
+     },
+     {
+        "name": "R2-D2",
+        "height": "96",
+        "mass": "32",
+        "hair_color": "n/a",
+        "skin_color": "white, blue",
+        "eye_color": "red",
+        "birth_year": "33BBY",
+        "gender": "n/a",
+        "homeworld": "https://swapi.co/api/planets/8/",
+        "films": [
+        "https://swapi.co/api/films/2/",
+        "https://swapi.co/api/films/5/",
+        "https://swapi.co/api/films/4/",
+        "https://swapi.co/api/films/6/",
+        "https://swapi.co/api/films/3/",
+        "https://swapi.co/api/films/1/",
+        "https://swapi.co/api/films/7/"
+     },
+     {
+        "name": "Darth Vader",
+        "height": "202",
+        "mass": "136",
+        "hair_color": "none",
+        "skin_color": "white",
+        "eye_color": "yellow",
+        "birth_year": "41.9BBY",
+        "gender": "male",
+        "homeworld": "https://swapi.co/api/planets/1/",
+        "films": [
+        "https://swapi.co/api/films/2/",
+        "https://swapi.co/api/films/6/",
+        "https://swapi.co/api/films/3/",
+        "https://swapi.co/api/films/1/"
+     },
+     {
+        "name": "Leia Organa",
+        "height": "150",
+        "mass": "49",
+        "hair_color": "brown",
+        "skin_color": "light",
+        "eye_color": "brown",
+        "birth_year": "19BBY",
+        "gender": "female",
+        "homeworld": "https://swapi.co/api/planets/2/",
+        "films": [
+        "https://swapi.co/api/films/2/",
+        "https://swapi.co/api/films/6/",
+        "https://swapi.co/api/films/3/",
+        "https://swapi.co/api/films/1/",
+        "https://swapi.co/api/films/7/"
+     }
+     ];
+
+app.get('/',function(req,res){
+    res.send("main route");
+
+app.listen(3000,function(req,res){
+    console.log('Express listening on port 3000');
+});
